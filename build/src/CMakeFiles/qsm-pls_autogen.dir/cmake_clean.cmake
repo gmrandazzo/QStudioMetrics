84daffc437b6070377fa053bce185069f89865f3
gmrandazzo <gmrandazzo@gmail.com>
2019-11-22 13:15:55 +0100

    Cleanup

--- build/src/CMakeFiles/qsm-pls_autogen.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/qsm-pls_autogen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/qsm-pls_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/src/CMakeFiles/testimportdvector_autogen.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/testimportdvector_autogen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/testimportdvector_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
